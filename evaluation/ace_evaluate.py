7fd40224d6153e305369026ce4a71bceada20272
Niketan Chandarana <42366580+niketan16@users.noreply.github.com>
2023-04-18 13:14:21 -0700

    moved the evaluation folder outside of the code folder

--- evaluation/ace_evaluate.py
+++ evaluation/ace_evaluate.py
@@ -1,8 +1,8 @@
 import evaluate_model
 import sys
 import os
-sys.path.insert(1, '../../backend/ACE/')
-sys.path.insert(1, '../backend/ACE/')
+# sys.path.insert(1, '../../backend/ACE/')
+sys.path.insert(1, '../code/backend/ACE/')
 import train_ace
 
 model, tokenizer = train_ace.load_model()
